ce22d87e5f90a217985f2449988c0438de2d1ed3
flan02 <chanivetdan@hotmail.com>
2024-07-20 22:19:08 -0300

    Nextjs 14.2.4 installed. From now the project starts!

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl text-purple-700 mt-4 pl-4">Blog site</h1>
+    </main>
+  );
+}
